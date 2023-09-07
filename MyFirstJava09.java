ce575c1eeab09e036be1f3181d92f38c94ce7858
erikaatthacarya <erikaputric48@gmail.com>
2023-09-07 14:17:55 +0700

    Mencoba Alur Balik

---
+++ MyFirstJava09.java
@@ -0,0 +1,12 @@
+public class  MyFirstJava09 {
+    public static void main(String[] args) {
+        
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.print("\nNAMA\t: Erika Atthacarya Putri Cahyani");
+        System.out.print("\nABSEN\t: 09");
+        System.out.print("\nKELAS\t: 1F");
+        System.out.print("\nPRODI\t: Sistem Informasi Bisnis");
+        System.out.print("\nJurusan\t: Teknologi Informasi");
+
+    }
+}
